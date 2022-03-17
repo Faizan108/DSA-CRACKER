061a02b7244737867b3187a0e50746f3c61f4ba0
Faizan Saifi <saniasaifi108@gmail.com>
2022-03-17 23:48:19 +0530

    Conversion of sequence in to a mobile number

---
+++ String/mobile_number2.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class mobile_number2 {
+    public static void main(String args[])
+    {
+        Scanner in=new Scanner(System.in);
+        String st=in.nextLine();
+        String s="";
+        String arr[]={"2","22","222","3","33","333","4","44","444","5","55","555","6","66","666","7","77","777","7777","8","88","888","9","99","999","9999"};
+        for(int i=0;i<st.length();i++)
+        {
+            char c=st.charAt(i);
+            if(c==' ')
+            {
+                s+='0';
+            }
+            else{
+                int pos=(int)c-65;
+                s=s+arr[pos];
+            }
+        }
+        System.out.println(s);
+        in.close();
+        
+    }
+}
