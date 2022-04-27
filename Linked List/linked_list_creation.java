18cd1c5593d559724f6e90cac9c2672fb2441b7c
Faizan Saifi <saniasaifi108@gmail.com>
2022-04-28 00:00:04 +0530

    linked list creation

---
+++ Linked List/linked_list_creation.java
@@ -0,0 +1,52 @@
+import java.util.*;
+class node{
+    int data;
+    node next;
+    node(int data)
+    {
+        this.data=data;
+        this.next=null;
+    }
+}
+class linked_list_creation{
+    public static node add(node head,int n)
+    {
+        node newnode=new node(n);
+        node temp=head;
+        if(head==null)
+        {
+            head=newnode;
+            return head;
+        }
+        while(temp.next!=null)
+        {
+            temp=temp.next;
+        }
+        temp.next=newnode;
+        return head;
+    }
+    public static node print(node head)
+    {
+        node temp=head;
+        while(temp!=null)
+        {
+            System.out.print(temp.data+"->");
+            temp=temp.next;
+        }
+        System.out.println("null");
+        return head;
+    }
+    public static void main(String[] args)
+    {
+        Scanner in=new Scanner(System.in);
+        node head=null;
+        head=add(head,10);
+        head=add(head,20);
+        head=add(head,30);
+        head=add(head,40);
+        head=add(head,50);
+        head=print(head);
+        in.close();
+
+    }
+}
